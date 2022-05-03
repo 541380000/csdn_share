3eda97c982cade6d5356bd49a4a99307ccb1a62c
541380000 <37223331+541380000@users.noreply.github.com>
2022-05-03 18:17:40 +0800

    Create windows_shm.py

---
+++ shared_memory_share_dataframe/windows_shm.py
@@ -0,0 +1,23 @@
+import mmap
+from io import StringIO
+from struct import unpack
+import pandas as pd
+
+# 注意这里写的大小，一定要不大于C++中开启的共享内存大小，否则映射失败
+SHARE_MEMORY_FILE_SIZE_BYTES = 100*1024
+mmap_file = mmap.mmap(-1, SHARE_MEMORY_FILE_SIZE_BYTES, "shared_memory1", mmap.ACCESS_READ)
+buffer_size = unpack("i", mmap_file.read(4))
+print(f"Buffer size: {buffer_size}")
+
+ex_factor = pd.read_csv(StringIO(mmap_file.read(buffer_size[0]).decode()), index_col=0)
+print(ex_factor)
+
+"""
+结果：
+Buffer size: (36,)
+       a  b  c
+index         
+0      1  1  1
+1      2  2  2
+2      3  3  3
+"""
